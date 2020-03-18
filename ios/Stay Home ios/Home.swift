a72003fbef0c34c8738fa2320be29b7fc3cab47a
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-03-18 18:07:39 -0400

    Set up LocationManager

--- ios/Stay Home ios/Home.swift
+++ ios/Stay Home ios/Home.swift
@@ -13,45 +13,58 @@ struct Home: View {
     
     @State private var centerCoordinate = CLLocationCoordinate2D()
     
+    @ObservedObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
     var body: some View {
         ZStack {
             MapView(centerCoordinate: $centerCoordinate)
                 .edgesIgnoringSafeArea(.all)
-            
-            // Home circle
+            HStack {
+                Text("latitude: \(userLatitude)")
+                Text("longitude: \(userLongitude)")
+            }
+            // Current location circle
             Circle()
                 .fill(Color.blue)
                 .opacity(0.3)
                 .frame(width: 32, height: 32)
             
             VStack{
-            // Virus guy button
-            HStack{
-                Spacer()
-                Circle()
-                    .fill(Color.blue)
-                    .frame(width: 32, height: 32)
-                .padding(25)
-            }
-            
-            // Change home button
-            VStack {
-                Spacer()
-                HStack {
-                    Button(action: {
-                        // create a new location
-                    }) {
-                        Text("Change Home")
-                    }
-                    .padding()
-                    .frame(width: 300, height: 50, alignment: .center)
-                    .background(Color(UIColor(red:0.94, green:0.69, blue:0.69, alpha:1.00)))
-                    .cornerRadius(25)
-                    .foregroundColor(.white)
-                    .font(.title)
+                // Virus guy button
+                HStack{
+                    Spacer()
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 32, height: 32)
+                        .padding(25)
                 }
-                Spacer().frame(height: 50)
-            }
+                
+                // Change home button
+                VStack {
+                    Spacer()
+                    HStack {
+                        Button(action: {
+                            // create a new location
+                        }) {
+                            Text("Change Home")
+                        }
+                        .padding()
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .background(Color(UIColor(red:0.94, green:0.69, blue:0.69, alpha:1.00)))
+                        .cornerRadius(25)
+                        .foregroundColor(.white)
+                        .font(.title)
+                    }
+                    Spacer().frame(height: 50)
+                }
             }
         }
     }
---
+++ ios/Stay Home ios/LocationManager.swift
@@ -0,0 +1,58 @@
+//
+//  LocationManager.swift
+//  
+//
+//  Created by Vishnu Ravi on 3/18/20.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, ObservableObject {
+    
+    override init(){
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    @Published var locationStatus: CLAuthorizationStatus?
+    
+    @Published var lastLocation: CLLocation?
+    
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+
+    }
+    
+    private let locationManager = CLLocationManager()
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.locationStatus = status
+        print(#function, statusString)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.lastLocation = location
+        print(#function, location)
+    }
+
+}
