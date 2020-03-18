2d29c970b30d64a965c30c72a5920f8886ed97bb
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-03-18 14:54:29 -0400

    Add MapView and buttons

--- ios/Stay Home ios/Home.swift
+++ ios/Stay Home ios/Home.swift
@@ -7,10 +7,53 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct Home: View {
+    
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    
     var body: some View {
-        Text("Map")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate)
+                .edgesIgnoringSafeArea(.all)
+            
+            // Home circle
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack{
+            // Virus guy button
+            HStack{
+                Spacer()
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 32, height: 32)
+                .padding(25)
+            }
+            
+            // Change home button
+            VStack {
+                Spacer()
+                HStack {
+                    Button(action: {
+                        // create a new location
+                    }) {
+                        Text("Change Home")
+                    }
+                    .padding()
+                    .frame(width: 300, height: 50, alignment: .center)
+                    .background(Color(UIColor(red:0.94, green:0.69, blue:0.69, alpha:1.00)))
+                    .cornerRadius(25)
+                    .foregroundColor(.white)
+                    .font(.title)
+                }
+                Spacer().frame(height: 50)
+            }
+            }
+        }
     }
 }
 
---
+++ ios/Stay Home ios/MapView.swift
@@ -0,0 +1,41 @@
+//
+//  MapView.swift
+//  Stay Home ios
+//
+//  Created by Vishnu Ravi on 3/18/20.
+//  Copyright © 2020 Stay Home. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.centerCoordinate = mapView.centerCoordinate
+        }
+    }
+}
