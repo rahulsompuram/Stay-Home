13ac33587bd8a3feb0d5f4d982d0f4462c9ba4ea
Rahul Sompuram <sompuram@sas.upenn.edu>
2020-03-18 11:51:31 -0400

    native ios setup

---
+++ ios/Stay Home ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stay Home ios
+//
+//  Created by Rahul Sompuram on 3/18/20.
+//  Copyright © 2020 Stay Home. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
