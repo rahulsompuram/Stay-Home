1d9d18f8b1eb25bf2a72961d4df465fd6f946338
Trevor Pennypacker <trevp@seas.upenn.edu>
2020-03-18 23:45:20 -0400

    leaderboard working but not pretty

--- ios/Stay Home ios/Leaderboard.swift
+++ ios/Stay Home ios/Leaderboard.swift
@@ -7,10 +7,60 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
+
+var ref: DatabaseReference!
+
+struct Row: View {
+    var rank: UInt
+    var username: String
+    var points: IntegerLiteralType
+    var id: UUID = UUID()
+    
+    var body: some View {
+        Text("#\(rank) || \(username) || \(points)pts")
+//        HStack {
+//            Text("#\(rank)")
+//            Text("\(username)")
+//            Text("\(points)pts").align
+//        }
+    }
+}
 
 struct Leaderboard: View {
+    
+    @State var rows: [Row] = []
+        
     var body: some View {
-        Text("Leaderboard")
+        List(self.rows, id: \.id) { item in
+            Row(rank: item.rank, username: item.username, points: item.points)
+        }.onAppear() {
+            
+            UITableView.appearance().separatorColor = .clear
+            
+            self.rows = []
+            ref = Database.database().reference()
+            
+            // make this max out at ~100 results
+            ref.child("Leaderboard").queryOrderedByValue().observeSingleEvent(of: .value) { (snapshot) in
+                
+                var rank = snapshot.childrenCount
+                
+                for rows in snapshot.children.allObjects as! [DataSnapshot] {
+                    
+                    var username = rows.key
+                    username = username.padding(toLength: 12, withPad: " ", startingAt: 0)
+                    let points = rows.value as! IntegerLiteralType
+    
+                self.rows.append(Row(rank: rank, username: username, points: points))
+                    
+                    rank -= 1
+                }
+                
+                self.rows.reverse()
+            }
+            
+        }
     }
 }
 
