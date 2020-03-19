9a7e94eb1ee87bdd36a6b1f8716a21595bf259bc
Rahul Sompuram <sompuram@sas.upenn.edu>
2020-03-19 19:16:31 -0400

    sprite view init

---
+++ ios/Stay Home ios/SpriteView.swift
@@ -0,0 +1,21 @@
+//
+//  SpriteView.swift
+//  Stay Home ios
+//
+//  Created by Rahul Sompuram on 3/19/20.
+//  Copyright © 2020 Stay Home. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpriteView()
+    }
+}
